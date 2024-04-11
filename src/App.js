b70e21d74ae64ec8abd95e45b1bae7e2fd7d7ac1
Ush7 <ush.shilp@gmail.com>
2024-04-11 13:55:36 -0400

    Create React app and removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello to the other side</h1>
+      </header>
+      <footer>
+        {" "}
+        This project coded by{" "}
+        <a href="/" target="_blank">
+          Unisha Shilpakar
+        </a>
+        and is{" "}
+        <a href="/" target="_blank">
+          open-sourced
+        </a>{" "}
+        on Github
+      </footer>
+    </div>
+  );
+}
+
+export default App;
